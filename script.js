3e7b67c8c367a4e0d201f0e4e8f9f590930ffc7e
manwithgills <chris.dalton1207@gmail.com>
2016-06-19 22:43:18 -0400

    added content to index file and added css and script for weather app

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  //test for Geolocation in Browser
+  if ("geolocation" in navigator) {
+    console.log("Geolocation is available");
+  } else {
+    console.log("Geolocation is NOT available");
+  };
+  //Get current coordinates 
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position.coords.latitude + "," + position.coords.longitude);
+//set countries not using metric system
+var imperialCountries = ["US", "Liberia", "Myanmar"];
+      //test array
+      console.log(imperialCountries);    
+    
+    //Get City and State and place into location field.
+    $.getJSON('//api.wunderground.com/api/*d9631bbbc8a29740/geolookup/q/' + position.coords.latitude + ',' + position.coords.longitude + '.json', function(data) {
+ 
+ $("h1").append(data.location.city + ", " + data.location.state);
+
+      //Get Weather Data 
+      $.getJSON('//api.wunderground.com/api/*d9631bbbc8a29740/conditions/q/'+data.location.state+'/'+data.location.city+'.json', function(conditions) {
+
+        //Temp
+       $(".tempBox").append(conditions.current_observation.temp_f);
+        
+        
+        
+        //windspeed
+        $("#windSpeed").append(conditions.current_observation.wind_mph+" MPH");
+
+        //icon
+$("#iconBox").css(background-image, "url:(+'conditions.current_observation.icon_url'+)");
+
+      })
+    });
+
+  });
+
+});
